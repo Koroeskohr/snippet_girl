e11e499eff331bc5f1cf7b752ebe6ca8acf3f704
Victor Viale <somebodywas@gmail.com>
2016-10-31 16:56:32 +0100

    Improve errors to be able to receive custom message

--- lib/snippet_girl/errors.rb
+++ lib/snippet_girl/errors.rb
@@ -1,12 +1,12 @@
 module SnippetGirl
   class NotImplementedError < StandardError
-    def message(message)
+    def message(message = nil)
       message || 'Method must be implemented'
     end 
   end
 
   class FormatError < StandardError
-    def message(message)
+    def message(message = nil)
       message || 'JSON was not a valid list'
     end
   end
